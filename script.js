48b6523032957c6341ca314aee4e0787a03d7f28
Shagan742 <526ssangha@frhsd.com>
2025-12-17 13:45:54 -0500

    began to work on project, trying to design html
    Co-authored-by: ktsim88 <ktsim88@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //make a wheel of names
+const vue_app = Vue.createApp({
+    created() {
+        fetch('names.json', 'items.json').then(response => response.json()).then(json => {
+                  this.names = json
+                  this.items = json
+            })
+    },
+    data() {
+        return {
+            names: [],//names array
+            items: []//items array
+        }
+    },
+    methods: {
+        //adding all methods here
+        startGame: function(start) {
+            //show the wheel+input field for names
+            start=true;
+        }
+    }
+    
+})
