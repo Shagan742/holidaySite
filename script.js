6556d1b1111c9261beac191d16f93e5bedfc2760
Shagan742 <526ssangha@frhsd.com>
2025-12-19 01:31:50 +0000

    wheel mostly works and we just need to fix background color + 1-2 slice issues, generated 60 items in items.json that need images replaced, used vue to kinda save inputs in js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//just commenting for my own note
 const vue_app = Vue.createApp({
     created() {
         fetch('names.json').then(response => response.json()).then(json => {
@@ -9,22 +9,45 @@ const vue_app = Vue.createApp({
                   this.items = json
             })
     },
-    data() {
-        return {
+    data() {//this is the data
+        return { 
             names: [],//names array
             items: [],//items array
-            start: false
+            start: false,
+            putName: "",
+            userName:"",
+            budget:0 
         }
     },
-    methods: {
+    methods: { //does things with data and the math of the computed things
         //adding all methods here
         startGame: function(starT) {
             //show the wheel+input field for names
-            this.start=starT;
+            this.start=starT; //start changes from false to true, activating the d-nones and d-blocks to change layout from loading screen to wheel of names
+            console.log(this.userName) //works
+            console.log(this.budget) //works
+        },
+        putNames: function(){
+            const theName=this.putName.split("\n").map(name => name.trim()).filter(Boolean) //the big string in putName i accumulate gets split up then trimmed + empty lines are ignored
+            this.names.push(...theName) //the ...means to change theName from an array to a bunch of strings, basically removing its [] symbols
+            console.log(this.names); //making sure my array works and it does
+            this.putName=" " //clear the input box to stop the slices from somehow multiplying
+        }
+    },
+    computed: { //does math with the data, returning numbers i can use later 
+        rotateSlice: function() {
+            if(this.names.length>0) {
+                return 360/(this.names.length) //it rotates new slices to avoid overlapping
+            } else {
+                return 0; //no names = no rotate
+            }
+        },
+        sliceSize: function() {
+            const angle= 2*Math.PI/(this.names.length)//converts angle from degrees (default) to radians
+            return 2 * 250 * Math.tan(angle / 2)   // uses some math to determine all slices' widths after a new slice is added
             
         }
-    }
-    
+    }    
 })
 
 vue_app.mount('#app')
--- wheel.js
+++ wheel.js
@@ -1,12 +1,13 @@
 //make a wheel of names
 let container = document.querySelector('.wheelContainer');
 let btn = document.getElementById('spin');
-let number = Math.ceil(Math.random() * 1000);
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+let numberOne = Math.ceil(pickName);
 
 btn.onclick = function () {
     container.style.transition = 'transform 2s ease-out';
-    container.style.transform = `rotate(${number}deg)`;
-    number += Math.ceil(Math.random() * 1000); // Increase the rotation angle for the next spin
+    container.style.transform = `rotate(${numberOne}deg)`;
+    numberOne += Math.ceil(Math.random() * 1000); // Increase the rotation angle for the next spin
 
 }
+
+//we want this to randomly land on a name
