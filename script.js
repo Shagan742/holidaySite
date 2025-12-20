53aa20b5878416dce995d687a1b6d13a71379ead
Shagan742 <526ssangha@frhsd.com>
2025-12-20 01:19:00 +0000

    deleted everything about names because it's too effortful + replaced w items at this point, beginning to fix the wheel and make it do things
    Co-authored-by: ktsim88 <ktsim88@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,53 +1,48 @@
 //just commenting for my own note
 const vue_app = Vue.createApp({
     created() {
-        fetch('names.json').then(response => response.json()).then(json => {
-                  this.names = json
-            })
-
-            fetch('items.json').then(response => response.json()).then(json => {
-                  this.items = json
-            })
+        fetch('items.json').then(response => response.json()).then(json => {
+            this.items = json
+        })
     },
     data() {//this is the data
-        return { 
-            names: [],//names array
+        return {
             items: [],//items array
+            itemsToPut: [],
             start: false,
             putName: "",
-            userName:"",
-            budget:0 
+            userName: "",
+            budget: 0,
+            colors: ["#F9F9F8", "#FFFFFF", "#FBFBFB", "#A9D9C6", "#CCD8E9", "#A0B3C6", "#D2D6D9", "#C2CBCA",
+                "#707E7F", "#5F5388", "#7A7EA1", "#7A9AAC", "#C4CBD4", "#497abbff", "#2b4c77ff", "#C4CBD4"]
         }
     },
     methods: { //does things with data and the math of the computed things
         //adding all methods here
-        startGame: function(starT) {
+        startGame: function (starT) {
             //show the wheel+input field for names
-            this.start=starT; //start changes from false to true, activating the d-nones and d-blocks to change layout from loading screen to wheel of names
+            this.start = starT; //start changes from false to true, activating the d-nones and d-blocks to change layout from loading screen to wheel of items
             console.log(this.userName) //works
             console.log(this.budget) //works
-        },
-        putNames: function(){
-            const theName=this.putName.split("\n").map(name => name.trim()).filter(Boolean) //the big string in putName i accumulate gets split up then trimmed + empty lines are ignored
-            this.names.push(...theName) //the ...means to change theName from an array to a bunch of strings, basically removing its [] symbols
-            console.log(this.names); //making sure my array works and it does
-            this.putName=" " //clear the input box to stop the slices from somehow multiplying
         }
     },
     computed: { //does math with the data, returning numbers i can use later 
-        rotateSlice: function() {
-            if(this.names.length>0) {
-                return 360/(this.names.length) //it rotates new slices to avoid overlapping
+        rotateSlice: function () {
+            if (this.filterItems.length > 0) {
+                return 360 / (this.filterItems.length) //it rotates new slices to avoid overlapping
             } else {
                 return 0; //no names = no rotate
             }
         },
-        sliceSize: function() {
-            const angle= 2*Math.PI/(this.names.length)//converts angle from degrees (default) to radians
+        sliceSize: function () {
+            const angle = 2 * Math.PI / (this.filterItems.length)//converts angle from degrees (default) to radians
             return 2 * 250 * Math.tan(angle / 2)   // uses some math to determine all slices' widths after a new slice is added
-            
+
+        },
+        filterItems: function () {
+            return this.items.filter(item => item.price <= this.budget)
         }
-    }    
+    }
 })
 
 vue_app.mount('#app')
--- wheel.js
+++ wheel.js
@@ -1,13 +1,13 @@
 //make a wheel of names
 let container = document.querySelector('.wheelContainer');
-let btn = document.getElementById('spin');
-let numberOne = Math.ceil(pickName);
+let spin = document.getElementById('spin');
+let numberOne = 10000;
 
-btn.onclick = function () {
-    container.style.transition = 'transform 2s ease-out';
+spin.onclick = function () {
+    container.style.transition = 'transform 5s ease-out';
     container.style.transform = `rotate(${numberOne}deg)`;
-    numberOne += Math.ceil(Math.random() * 1000); // Increase the rotation angle for the next spin
+    numberOne +=Math.random()*1000; // Increase the rotation angle for the next spin
 
 }
 
-//we want this to randomly land on a name
+//we want this to randomly land on an item
