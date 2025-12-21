e64f88676ce643e7bccd099e824973d488a5fb7d
ktsim88 <526ktsim@frhsd.com>
2025-12-21 01:42:52 +0000

    I MADE THE WHEEL SPIN PROPERLY!!!!!!!!!!

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 //just commenting for my own note
 const vue_app = Vue.createApp({
     created() {
-        fetch('items.json').then(response => response.json()).then(json => {
-            this.items = json
+        fetch('items.json').then(response => response.json())
+            .then(json => {
+            this.items = json.map(item => ({
+                    ...item,//this copies all the data from the json file
+                    color: this.colors[Math.floor(Math.random() * this.colors.length)]//this adds a random color from the colors array to each item
+                }));
         })
     },
     data() {//this is the data
@@ -24,6 +28,23 @@ const vue_app = Vue.createApp({
             this.start = starT; //start changes from false to true, activating the d-nones and d-blocks to change layout from loading screen to wheel of items
             console.log(this.userName) //works
             console.log(this.budget) //works
+        },
+        spinWheel: function () {
+            //spin the wheel
+            const slices = this.filterItems.length;
+        if (slices < 2) return;//this is to make sure spinner wont spin if user only puts 1 dollar as budget
+
+        const sliceAngle = 360 / slices;//this is the angle of each slice
+            const selectedIndex = Math.floor(Math.random() * slices);
+            const selectedItem = this.filterItems[selectedIndex];
+        const rotation =
+            360 * 5 + (360 - (selectedIndex * sliceAngle + sliceAngle / 2)); //this is the rotation of the wheel, it rotates 5 times then stops at a random slice
+
+        const wheel = document.getElementById('wheel');
+        wheel.style.transition = 'transform 5s ease-out';
+            wheel.style.transform = `rotate(${rotation}deg)`;
+            this.itemsToPut.push(selectedItem);//this adds the selected item to the array of items to put
+
         }
     },
     computed: { //does math with the data, returning numbers i can use later 
@@ -35,6 +56,7 @@ const vue_app = Vue.createApp({
             }
         },
         sliceSize: function () {
+            if (this.filterItems.length < 2) return 0;//this is the width of the slices
             const angle = 2 * Math.PI / (this.filterItems.length)//converts angle from degrees (default) to radians
             return 2 * 250 * Math.tan(angle / 2)   // uses some math to determine all slices' widths after a new slice is added
 
