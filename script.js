9c6e1b3c37166f8a19c1252ff5480f6f108231e0
Shagan742 <526ssangha@frhsd.com>
2025-12-21 19:30:01 +0000

    modal works, tailored spinwheel code
    Co-authored-by: ktsim88 <ktsim88@users.noreply.github.com>

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const vue_app = Vue.createApp({
     created() {
         fetch('items.json').then(response => response.json())
             .then(json => {
-            this.items = json.map(item => ({
+                this.items = json.map(item => ({
                     ...item,//this copies all the data from the json file
                     color: this.colors[Math.floor(Math.random() * this.colors.length)]//this adds a random color from the colors array to each item
                 }));
-        })
+            })
     },
     data() {//this is the data
         return {
@@ -16,7 +16,7 @@ const vue_app = Vue.createApp({
             start: false,
             putName: "",
             userName: "",
-            budget: 0,
+            budget: 2,
             colors: ["#F9F9F8", "#FFFFFF", "#FBFBFB", "#A9D9C6", "#CCD8E9", "#A0B3C6", "#D2D6D9", "#C2CBCA",
                 "#707E7F", "#5F5388", "#7A7EA1", "#7A9AAC", "#C4CBD4", "#497abbff", "#2b4c77ff", "#C4CBD4"],
             selectedGift: {}
@@ -32,39 +32,36 @@ const vue_app = Vue.createApp({
         },
         spinWheel: function () {
             //spin the wheel
-            const slices = this.filterItems.length;
-        if (slices < 2) return;//this is to make sure spinner wont spin if user only puts 1 dollar as budget
 
-        const sliceAngle = 360 / slices;//this is the angle of each slice
-            const selectedIndex = Math.floor(Math.random() * slices);
-            const selectedItem = this.filterItems[selectedIndex];
-        const rotation =
-            360 * 5 + (360 - (selectedIndex * sliceAngle + sliceAngle / 2)); //this is the rotation of the wheel, it rotates 5 times then stops at a random slice
 
-        const wheel = document.getElementById('wheel');
-        wheel.style.transition = 'transform 5s ease-out';
+            this.sliceSize;
+            const selectedIndex = Math.floor(Math.random() * this.filterItems.length);
+            const selectedItem = this.filterItems[selectedIndex];  //randomly choose item in the filterItems array
+            const rotation = this.rotateSlice; //this is the rotation of the wheel, it rotates 5 times then stops at a random slice
+            const wheel = document.getElementById('wheel');
+            wheel.style.transition = 'transform 5s ease-out'; //when spin session ends, wheel smoothly stops
             wheel.style.transform = `rotate(${rotation}deg)`;
             this.itemsToPut.push(selectedItem);//this adds the selected item to the array of items to put
             setTimeout(() => {
-    this.selectedGift = selectedItem;
-    // this.openModal();
-}, 5000);
+                this.selectedGift = selectedItem;
+                this.openModal();
+            }, 5000);
 
-            
+
         }
-//         ,
-//         openModal: function () {
-//     const myModal = new bootstrap.Modal(document.getElementById('myModal'));
-//     myModal.show();
-// }
+        ,
+        openModal: function () {
+            const myModal = new bootstrap.Modal(document.getElementById('myModal'));
+            myModal.show();
+        }
 
 
-        
+
     },
     computed: { //does math with the data, returning numbers i can use later 
         rotateSlice: function () {
             if (this.filterItems.length > 0) {
-                return 360 / (this.filterItems.length) //it rotates new slices to avoid overlapping
+                return 360 / (this.filterItems.length)*5 //it rotates new slices to avoid overlapping
             } else {
                 return 0; //no names = no rotate
             }
